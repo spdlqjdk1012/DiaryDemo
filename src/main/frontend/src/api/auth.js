f9719ecba22619c20c624cc5d15f704797290b02
Park Yu Ri <spdlqjdk1012@naver.com>
2025-02-15 23:36:09 +0900

    로그아웃추가

--- src/main/frontend/src/api/auth.js
+++ src/main/frontend/src/api/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/user';
+//const BASE_URL = 'http://localhost:8080/api/user';
+const BASE_URL = 'https://light-dolls-repair.loca.lt/api/user';
 
 export const login = async (email, password) => {
   try {
@@ -11,5 +12,21 @@ export const login = async (email, password) => {
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: '로그인 중 오류가 발생했습니다.' };
+  }
+};
+
+export const logout = async () => {
+  try {
+    const response = await axios.post(`${BASE_URL}/logout`);
+    
+    // 로그아웃 성공 시 로컬 스토리지의 인증 정보 삭제
+    localStorage.removeItem('token');
+    localStorage.removeItem('email');
+    localStorage.removeItem('nickname');
+    localStorage.removeItem('cuid');
+    
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: '로그아웃 중 오류가 발생했습니다.' };
   }
 };
--- src/main/frontend/src/pages/Test.js
+++ src/main/frontend/src/pages/Test.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Test = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);//token 있으면 true 없으면 false
+  }, []);
+
   const navigate = useNavigate();
   const handleLogin = () => {
+    navigate("/login");
+  }
+
+  const handleLogout = async () => {
+    await logout();
+    setIsLoggedIn(false);
     navigate("/login");
   }
   return (
     <div>
         main
-        <button onClick={handleLogin}>로그인</button>
+        {isLoggedIn ? (
+          <button onClick={handleLogout}>로그아웃</button>
+        ) : (
+          <button onClick={handleLogin}>로그인</button>
+        )}
+        
     </div>
   )
 }
